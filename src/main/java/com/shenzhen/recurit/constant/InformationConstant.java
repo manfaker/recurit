123d03679a491a37a5eff0ea40de9d62c6af387f
manfaker <1050484730@qq.com>
2020-03-11 00:32:22 +0800

    遗漏提交

---
+++ src/main/java/com/shenzhen/recurit/constant/InformationConstant.java
@@ -0,0 +1,39 @@
+package com.shenzhen.recurit.constant;
+
+public interface InformationConstant {
+
+    /**
+     * 手机验证部分配置
+     */
+    // 设置超时时间-可自行调整
+    String  DEFAULT_CONNECT_TIME_OUT  = "sun.net.client.defaultConnectTimeout";
+    String  DEFAULT_READ_TIME_OUT = "sun.net.client.defaultReadTimeout";
+    String  TIME_OUT = "10000";
+    // 初始化ascClient需要的几个参数
+    String PRODUCT = "Dysmsapi";   // 短信API产品名称（短信产品名固定，无需修改）
+    String DOMAIN = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
+    //
+    String ACCESS_KEY_ID = "TFRBSTRGZ0VaMTlOR1hwZlFVOGtzZ2d5";// accessKeyId
+    String ACCESS_KEY_SECRET = "dmJDM3hRRE9oRWNLbzR2V0Q5N2phRTY4aFVYSXFP";//accessKeySecret
+    // 必填:短信签名-可在短信控制台中找到
+    String SIGN_NAME = "腾雾";// 短信签名
+    // 必填:短信模板-可在短信控制台中找到
+    String TEMPLATE_CODE = "U01TXzE4MzI2MDI3NA=="; // 短信模板
+
+    //邮箱验证部分配置
+    String SPRING_MAIL_HOST = "smtp.qq.com";
+    String spring_mail_username = "MTA1MDQ4NDczMEBxcS5jb20=";
+    String spring_mail_password = "cHJvcXJ2cnB4bWxmYmZhaQ==";//不是密码，是授权码
+    String spring_mail_port = "587";
+    String spring_mail_smtp_auth = "true";
+
+    //header 请求头
+    String AUTH_USER = "auth-user";
+
+    //缓存同步时间
+    long INITIAL_TIME=946691076849l;
+    String KEY_DICTIONARY_TIME = "dictionary_time";
+
+
+
+}
---
+++ src/main/java/com/shenzhen/recurit/constant/OrdinaryConstant.java
@@ -0,0 +1,23 @@
+package com.shenzhen.recurit.constant;
+
+public interface OrdinaryConstant {
+
+    String IS_BLACK = "";
+    //常用符号
+    String SYMBOL_1 = "@";
+    String SYMBOL_2 = "-";
+    String SYMBOL_3 = "_";
+    //日期格式
+    String YYYYMMDD = "yyyyMMdd";
+    String YYYY_MM_DD = "yyyy-MM-dd";
+    String YYYY_MM_DD_S = "yyyy/MM/dd";
+    String YYYY_MM_DD_ZH = "yyyy年MM月dd日";
+    String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+    String YYYY_MM_DD_H24_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd hh:mm:ss";
+    String YYYY_MM_DD_H24_MM_SS_S = "yyyy/MM/dd HH:mm:ss";
+    String YYYY_MM_DD_HH_MM_SS_S = "yyyy/MM-/dd hh:mm:ss";
+    String YYYY_MM_DD_H24_MM_SS_ZH = "yyyy年MM月dd日 HH时mm分ss秒";
+    String YYYY_MM_DD_HH_MM_SS_ZH = "yyyy年MM月dd日 HH时mm分ss秒";
+}
+
